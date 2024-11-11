a75e590694331b77378c61cd4d355aa67613c970
Davi Fernandes <davi.fer159@gmail.com>
2024-11-11 16:33:49 -0300

    add js

--- assets/scrips.js
+++ assets/scrips.js
@@ -1,1 +1,32 @@
+let dolar = 5.7;
 
+const usdInput = document.querySelector("#usd");
+const brlInput = document.querySelector("#brl");
+
+usdInput.addEventListener("keyup", () => {
+  convert("usd-to-brl");
+});
+
+brlInput.addEventListener("keyup", () => {
+  convert("brl-to-usd");
+});
+
+function fixValue(value) {
+  let numberFormat = value.replace(",", ".");
+  let resultNumber = Number(numberFormat);
+
+  return resultNumber;
+};
+
+function convert(type) {
+  if (type == "usd-to-brl") {
+    let fixedValue = fixValue(usdInput.value);
+     
+    let result = fixedValue * dolar;
+    result.toFixed(2);
+  }
+
+  if (type == "brl-to-usd") {
+    console.log("Converter de real para dolar!");
+  }
+}
